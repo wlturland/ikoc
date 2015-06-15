2f38d7cc4327f59a0029a52371a3f9b0853c7b31
Leigh Turland <wlturland@gmail.com>
2015-06-15 15:39:46 -0500

    Added paperclip

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ActiveRecord::Base
   belongs_to :category
+
+  has_attached_file :image, :styles => { 
+  	:medium => "300x300#", 
+  	:thumb => "100x100#" }, 
+  	:default_url => "missing_product_:style.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150615161857) do
+ActiveRecord::Schema.define(version: 20150615203426) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -25,8 +25,12 @@ ActiveRecord::Schema.define(version: 20150615161857) do
     t.integer  "quantity"
     t.text     "description"
     t.integer  "category_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id"
