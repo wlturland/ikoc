97aa2d10686332bb8c81bf1f465f1f8cb35fd895
Leigh Turland <wlturland@gmail.com>
2015-06-15 19:40:28 -0500

    Updated Cart security

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,6 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
 
   # GET /carts
   # GET /carts.json
@@ -66,6 +67,12 @@ class CartsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_cart
       @cart = Cart.find(params[:id])
+    end
+
+    # Redirects invalid carts back to All Items view
+    def invalid_cart
+     logger.error "Attempt to access invalid cart #{params[:id]}"
+     redirect_to storefront_all_items_path, notice: 'This is not the cart you are looking for.'
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
